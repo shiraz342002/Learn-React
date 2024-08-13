ad093de52aee1baeeec8dba2a90f0f21c4536d0d
Shiraz Mazhar <imshiraz007@gmail.com>
2024-08-13 15:54:39 +0500

    props

--- Tutorials/React-First-App/Getting-Started-With-React/src/App.jsx
+++ Tutorials/React-First-App/Getting-Started-With-React/src/App.jsx
@@ -1,11 +1,32 @@
 
 import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
-function App() {
-  
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+function AboutPage() {
   return (
     <>
-     
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+function App() {
+
+  return (
+    <>
+     <Navbar/>
+     <main>
+     <div>Welcome to my app</div>
+     <AboutPage/>
+     <MyButton />
+     </main>
+     <Footer/>
     </>
   )
 }
---
+++ Tutorials/React-First-App/Getting-Started-With-React/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
