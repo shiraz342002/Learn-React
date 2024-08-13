416f9c90eee1f8f8be98a1c368f76f6b8df8979e
Shiraz Mazhar <imshiraz007@gmail.com>
2024-08-13 17:27:27 +0500

    Just Basic fetching data

--- Tutorials/React-First-App/Getting-Started-With-React/src/App.jsx
+++ Tutorials/React-First-App/Getting-Started-With-React/src/App.jsx
@@ -3,11 +3,16 @@ import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
+// function MyButton() {
+//   return (
+//     <button>I'm a button</button>
+//   );
+// }
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
 function AboutPage() {
   return (
     <>
@@ -16,19 +21,25 @@ function AboutPage() {
     </>
   );
 }
-function App() {
 
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+}
+function App() {
   return (
+    // https://youtu.be/S4VH8hddg8c?t=965
     <>
      <Navbar/>
      <main>
      <div>Welcome to my app</div>
-     <AboutPage/>
-     <MyButton />
+     <h1>{user.name}</h1>
+     <img src={user.imageUrl} alt="" />
+     <br />
      </main>
      <Footer/>
     </>
   )
 }
-
 export default App
