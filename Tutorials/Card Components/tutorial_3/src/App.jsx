15b3b22afe5776743d91e4b68a997075b7c102a6
Shiraz Mazhar <imshiraz007@gmail.com>
2024-08-15 11:52:30 +0500

    Card Component Project

---
+++ Tutorials/Card Components/tutorial_3/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./components/card"
+function App() {
+ 
+  return (
+    <>
+     <Card/>
+     <Card/>
+     <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ Tutorials/Card Components/tutorial_3/src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='card'>
+        <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbgk0yfCOe55931lf6q0osfhGRU-fnH8Im1g&s" alt="profile picture" />
+        <h2>Shiraz Mazhar</h2>
+        <p>I'm a Mern Stack Developer and play Video Games</p>
+    </div>
+  )
+}
+
+export default Card
